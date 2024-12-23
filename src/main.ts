1e3263a05f028ede16413a247091821957734000
ThanhTNV <thanhtran190304@gmail.com>
2024-12-23 11:57:23 +0700

    SCRUM-130: Infisical secret by cli

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
@@ -8,9 +9,11 @@ async function bootstrap() {
     .setVersion('1.0')
     .setDescription('The user authentication API description')
     .build();
-
+  const secret = process.env.DB_URL;
+  console.log(secret);
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
